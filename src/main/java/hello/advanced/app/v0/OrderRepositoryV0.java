fe98f5019d6f8a75aebdafb60aaab962a5f75931
ws-song <ws.song@nts-corp.com>
2024-01-04 12:45:00 +0900

    init

---
+++ src/main/java/hello/advanced/app/v0/OrderRepositoryV0.java
@@ -0,0 +1,14 @@
+package hello.advanced.app.v0;
+
+import org.springframework.stereotype.Repository;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+	public void save(String itemId) {
+		// 저장로직
+
+	}
+}
